d7f5358fd5bdbb214fe736d166753cdb9c854fca
linwei365 <linwei365@gmail.com>
2016-02-13 13:53:19 -0800

    prepare for segue send table index moc to the sendviewController initial fetch from coreData

--- CoreDataPracticeB/MainTableViewController.swift
+++ CoreDataPracticeB/MainTableViewController.swift
@@ -206,14 +206,26 @@ class MainTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+       
+        
+       let index = self.tableView.indexPathForSelectedRow
+        
+        let vc =  segue.destinationViewController as! SecondViewController
+        
+        vc.indexRow = index?.row
+        
+        vc.moc = managedObjectContext
+        
+        
     }
-    */
+    
 
 }
--- CoreDataPracticeB/SecondViewController.swift
+++ CoreDataPracticeB/SecondViewController.swift
@@ -7,13 +7,46 @@
 //
 
 import UIKit
-
+import CoreData
 class SecondViewController: UIViewController {
 
+    var indexRow: Int?
+    var instructors = [Instructor]()
+    var courses = [Course]()
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var courseTitleTextField: UITextField!
+    var moc: NSManagedObjectContext!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        loadData()
+        firstNameTextField.text = instructors[indexRow!].nameFirst
+        lastNameTextField.text = instructors[indexRow!].nameLast
+        courseTitleTextField.text = courses[indexRow!].title
+        
+        
+        
+        
+    }
+    
+    func loadData () {
+     
+        
+        let fetchRequest = NSFetchRequest(entityName: "Instructor")
+        let fetchRequestB = NSFetchRequest(entityName: "Course")
+        
+        try! instructors = moc!.executeFetchRequest(fetchRequest) as! [Instructor]
+        
+        try! courses = moc!.executeFetchRequest(fetchRequestB) as! [Course]
     }
 
     override func didReceiveMemoryWarning() {
